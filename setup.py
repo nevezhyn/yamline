81026c4390ea988664350449c9d5fa31701f31d0
Roman Nevezhyn <roman@nevezhyn.com>
2017-11-15 11:06:46 -0500

    - version changed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='yamline',
-    version='0.0.1',
+    version='0.0.2',
     packages=['yamline'],
     url='https://github.com/nevezhyn/yamline',
     license='',
